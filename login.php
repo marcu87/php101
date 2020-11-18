371bd1a89801a80bb7d50491b3642b96567a0210
Paula Moreno <paula.moreno@check24.es>
2020-11-18 19:51:12 +0100

    mostrar tabla de usuarios

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-$credencialesRegistradas = [
+$usuarios = [
     [
         'usuario' => 'Paula',
         'contraseña' => '123'
@@ -8,29 +8,63 @@ $credencialesRegistradas = [
     [
         'usuario' => 'Santi',
         'contraseña' => '321'
+    ],
+    [
+        'usuario' => 'Pepe',
+        'contraseña' => '321'
+    ],
+    [
+        'usuario' => 'Juan',
+        'contraseña' => '321'
     ]
 ];
 
 $usuario = $_POST['usuario'];
 $contraseña = $_POST['contraseña'];
+$usuarioLogueado = false;
 
-foreach ($credencialesRegistradas as $credencialRegistrada) {
-    if ($usuario == $credencialRegistrada['usuario'] && $contraseña == $credencialRegistrada['contraseña']) {
-        echo '¡Usuario logueado correctamente!';
-        die;
-
-        echo 'Credenciales incorrectas';
-
-        }
-    if ($usuario == $credencialRegistrada['usuario'] && $contraseña != $credencialRegistrada['contraseña']) {
-        echo '¡Contraseña incorrecta!';
-        die;
+foreach ($usuarios as $usuarioRegistrado) {
+    if ($usuario == $usuarioRegistrado['usuario'] && $contraseña == $usuarioRegistrado['contraseña']) {
+        $usuarioLogueado = true;
+        $mensaje = '¡Usuario logueado correctamente!';
+        break;
+    } elseif ($usuario == $usuarioRegistrado['usuario'] && $contraseña != $usuarioRegistrado['contraseña']) {
+        $mensaje = '¡Contraseña incorrecta!';
+        break;
+    } else {
+        $mensaje = 'El usuario no existe';
+        break;
     }
 }
+?>
 
-echo 'El usuario no existe';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+    <?php if ($usuarioLogueado == true): ?>
+        <table>
+            <tr>
+                <th>Nombre</th>
+            </tr>
+            
+            <?php foreach ($usuarios as $usuarioRegistrado): ?>
+                <tr>
+                    <td><?php echo $usuarioRegistrado['usuario']; ?></td>
+                </tr>
+            <?php endforeach; ?>
 
-
-// tarea
-// Si las credenciales son incorrectas pero el usuario existe, mostrar: Contraseña incorrecta,
-// Sino, mostrar: el usuario no existe
+        </table>
+    <?php endif; ?>
+</body>
+</html>
