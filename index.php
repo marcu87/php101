695f9c87faee839fb6c56b50504e2d7bf6f8c286
Paula Moreno <paula.moreno@check24.es>
2020-12-03 09:01:28 +0100

    Style / Footer / Header

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register form</title>
-</head>
-<body>
-    <a href="registerForm.php"><button> Nuevo registro </button>
-    <br>
-    <a href="loginForm.php"><button> Acceder </button>
-    <br>
-</body>
-</html>
+<?php
+
+require __DIR__ . '/header.php';
+
+require __DIR__ . '/footer.php';
--- loginForm.php
+++ loginForm.php
@@ -1,51 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
+require __DIR__ . '/header.php';
 
-    <br>
-    <br>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-</head>
-
-<body>
-        <div class="container">
-        <form action="login.php" method="POST">
-            <div class="form-group">
-                <label for="user">Usuario</label>
-                <input type="text" class="form-control" name="user" id="user" aria-describedby="emailHelp">
-                <small id="emailHelp" class="form-text text-muted">No compartiremos tu correo electrónico con nadie más</small>
-            </div>
-            <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="password" class="form-control" name="password" id="exampleInputPassword1">
-            </div>
-            <div class="form-group form-check">
-            </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
-            <br>
-            <br>
-            <label>¿Aún no tienes una cuenta? Regístrate haciendo click <a href="registerForm.php">aquí</a>.</label><br /><br /><br /><br />
-        </form>
-        <!-- 
-            <label for="user">Usuario o Email</label>
-            <input type="text" name="user" id="user">
-            <br>
+?>
+<div class="container">
+    <form action="login.php" method="POST">
+        <div class="form-group">
+            <label for="user">Usuario</label>
+            <input type="text" class="form-control" name="user" id="user" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">No compartiremos tu correo electrónico con nadie más</small>
+        </div>
+        <div class="form-group">
             <label for="password">Contraseña</label>
-            <input type="password" name="password" id="password">
-            <br>
-            <button type="submit">Enviar</button>
-            <br>
-            <label>¿Aún no tienes una cuenta? Regístrate haciendo click <a href="registerForm.php">aquí</a>.</label><br /><br /><br /><br />
-            </form>
-            -->
-    </div>
+            <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+        </div>
+        <div class="form-group form-check">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+        <br>
+        <br>
+        <label>¿Aún no tienes una cuenta? Regístrate haciendo click <a href="registerForm.php">aquí</a>.</label><br /><br /><br /><br />
+    </form>
+</div>
 </body>
 
 </html>
