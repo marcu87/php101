f3dcf568e5b18cda69b1df22701e2edd8ebe6f3a
Paula <paumogon@gmail.com>
2020-12-06 16:38:25 +0100

    Multiarray

--- practice/practice1.php
+++ practice/practice1.php
@@ -31,6 +31,30 @@ $array = [
   ],
 ];
 
-/**
- * loop through this array of arrays and show in screen each value, but only if the the value of 'show' is set to true 
- */
+$keys = array_keys($array);
+for($i = 0; $i < count($array); $i++) {
+    echo $keys[$i] . "{<br>";
+    foreach($array[$keys[$i]] as $key => $value) {
+        echo $key . " : " . $value . "<br>";
+    }
+    echo "}<br>";
+}
+
+//for ($i = 0; $i < count($array); $i++) {
+//  echo $array[];
+
+//var_dump($array[0]);
+
+
+
+//Definir índices de un subarray:
+  //$array[0] :: devuelve el array situado en el índice 0
+//Acceder a uno de los valores de un subarray:
+  //$subarray = $array[0]
+  //echo $subarray['value'];
+
+//$subArray = $array[0] $array[1] $array[2] $array[3] $array[4] $array[5] $array[6];
+
+//$subArray = $array[];
+
+//$subArray = array('0','1','2','3','4','5'); //Definición sub array
