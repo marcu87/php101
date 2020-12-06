e91a6a03e1840e9d9a68d113cedea4956efe9bf8
Paula <paumogon@gmail.com>
2020-12-06 18:34:26 +0100

    explode phrase into lines one by one word

--- practice/practice2.php
+++ practice/practice2.php
@@ -11,4 +11,19 @@
  * 
  * tip: look up the function explode() in php.net
  */
+
 $phrase = 'What Goes Up Must Come Down';
+$words = explode(" ", $phrase);
+echo $words[0];
+echo "<br>";
+echo $words[1];
+echo "<br>";
+echo $words[2];
+echo "<br>";
+echo $words[3];
+echo "<br>";
+echo $words[4];
+echo "<br>";
+echo $words[5];
+
+?> 
