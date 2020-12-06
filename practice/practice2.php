fb5150508227924eb9b9899bd350a57ca25ad95d
Paula <paumogon@gmail.com>
2020-12-06 19:05:50 +0100

    explode + brek into lines

--- practice/practice2.php
+++ practice/practice2.php
@@ -26,4 +26,20 @@ echo $words[4];
 echo "<br>";
 echo $words[5];
 
+echo "<br>";
+echo "Si utilizamos el loop:";
+echo "<br>";
+
+//$phrase = 'What Goes Up Must Come Down';
+//$words2 = explode(" ", $phrase);
+//foreach ($words2 as $value) {
+//    echo $value[];
+//}
+
+$phrase = 'What Goes Up Must Come Down';
+$words2 = explode(" ", $phrase);
+for ($i=0; $i < count($words2); $i++) {
+    print $words2[$i] . "<BR>";
+}
+
 ?> 
