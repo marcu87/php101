1a5ac88357938fe91bccb5f4a4e80d20bbc4a403
Paula <paumogon@gmail.com>
2020-12-06 19:12:12 +0100

    For - Foreach

--- practice/practice2.php
+++ practice/practice2.php
@@ -25,21 +25,26 @@ echo "<br>";
 echo $words[4];
 echo "<br>";
 echo $words[5];
-
-echo "<br>";
-echo "Si utilizamos el loop:";
 echo "<br>";
 
-//$phrase = 'What Goes Up Must Come Down';
-//$words2 = explode(" ", $phrase);
-//foreach ($words2 as $value) {
-//    echo $value[];
-//}
+echo "<br>";
+echo "Si utilizamos el loop: FOREACH";
+echo "<br>";
+
+$phrase = 'What Goes Up Must Come Down';
+$words2 = explode(" ", $phrase);
+foreach ($words2 as $value) {
+    echo $value . '<br>';
+}
+
+echo "<br>";
+echo "Si utilizamos el loop: FOR";
+echo "<br>";
 
 $phrase = 'What Goes Up Must Come Down';
 $words2 = explode(" ", $phrase);
 for ($i=0; $i < count($words2); $i++) {
-    print $words2[$i] . "<BR>";
+    print $words2[$i] . '<br>';
 }
 
 ?> 
